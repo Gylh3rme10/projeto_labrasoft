d46ef2fd32262aa8e4fe0f42b516ddd6e9540362
Rafael Sabino <20252160003@ifba.edu.br>
2026-06-18 16:18:07 -0300

    Semana1_Aula1_Guilherme

--- WebApplication1/Models/Bolsista.cs
+++ WebApplication1/Models/Bolsista.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Remoting.Messaging;
 
 namespace WebApplication1.Models
 {
@@ -11,16 +12,28 @@ namespace WebApplication1.Models
         public string Nome { get; set; }
 
         // TODO: Criar a propriedade para o CPF
+        public string CPF { get; set; }
 
         // TODO: Criar a propriedade para a Matrícula
+        public string Matricula { get; set; }
 
         // TODO: Criar a propriedade para a Data de Nascimento
+        public DateTime Data_Nascimento { get; set; }
 
         // TODO: Criar a propriedade para o Sexo
+        public char Sexo { get; set; }
 
         // TODO: Criar método com o resumo das informações contendo nome e matrícula
-
+        public string RetornarResumo()
+        {
+            return $"Nome: {Nome} + Matricula: {Matricula}";
+        }
         //TODO: Criar método que calcúla a idade do bolsista      
+        public int CalcularIdade()
+        {
+            int idade = DateTime.Now.Year - Data_Nascimento.Year;
 
+            return idade;
+        }
     }
 }
